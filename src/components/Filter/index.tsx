a65840b0c654fe2caa203c5e1e3a52682779a320
Rafaella <rafaella_soares_custodio_de_lima_1@atacadao.com.br>
2025-11-11 11:23:10 -0300

    Icones

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,22 +1,24 @@
 import { FilterStatus } from "@/types/FilterStatus";
 import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { styles } from "./styles";
+import { CircleCheck } from "lucide-react-native"
 
 type Props = TouchableOpacityProps & {
-  status: FilterStatus
-  isActive: boolean
+    status: FilterStatus
+    isActive: boolean
 }
 
 export function Filter({ status, isActive, ...rest }: Props) {
-  return (
-    <TouchableOpacity 
-      style={[styles.container, { opacity: isActive ? 1 : 0.5 }]} 
-      activeOpacity={0.8}
-      {...rest} 
-      >
-      <Text style={styles.title}>
-        { status === FilterStatus.DONE ? "Comprados" : "Pendentes" }
-      </Text>
-    </TouchableOpacity>
-  )
+    return (
+        <TouchableOpacity
+            style={[styles.container, { opacity: isActive ? 1 : 0.5 }]}
+            activeOpacity={0.8}
+            {...rest}
+        >
+            <CircleCheck size={18} color="#000" />
+            <Text style={styles.title}>
+                {status === FilterStatus.DONE ? "Comprados" : "Pendentes"}
+            </Text>
+        </TouchableOpacity>
+    )
 }
