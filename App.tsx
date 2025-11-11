b3a468ae67de6505a5a9055ed6d234540a1b7810
Rafaella <rafaella_soares_custodio_de_lima_1@atacadao.com.br>
2025-11-11 08:23:02 -0300

    Organiza a estrutura do proejto

--- App.tsx
+++
@@ -1,7 +0,0 @@
-import Home from "./src/app/Home";
-
-export default function App() {
-  return (
-    <Home />
-  );
-};
--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { styles } from "./styles"
 
-export default function Home() {
+export  function Home() {
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>
